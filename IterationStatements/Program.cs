3a45b5c2af75b94c6ec1748db7c345713dfa153d
Brian Shaw <1BrianShaw@gmail.com>
2023-02-07 15:53:51 -0500

    Exercise 2

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,44 +11,50 @@ namespace IterationStatements
                 // like the example below
 
                 //Create a List called "numbers" - DONE!
-                var numbers = new List<int>();
-            
+            var numbers = new List<int>();
+
             //-----START HERE------------------------------------------
-            //Create a variable of type int and name it num
-            //initialize the variable with a value of 0
+            //Create a variable of type int and name it num - DONE!
+            //initialize the variable with a value of 0 - DONE!
+            int num = 0;
 
 
+            // Create a do-while loop and use the template below: - DONE!
 
-            // Create a do-while loop and use the template below:
+        
 
+            do
             {
-                // Increment num by 1
+                num++;
 
-                // Then add num to the collection - numbers
-                    // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                numbers.Add(num);
 
-            } // <---- While your variable is less than 100
-
-
+            } while (num<100);
 
             // Create a while loop
-            // <--- While num is less than 200
+            // <--- While num is less than 200 - DONE!
             {
                 // Increment num by 1
                 // Then add num to the collection numbers
-                    //HINT: copy how this was done in the do while loop
-
+                //HINT: copy how this was done in the do while loop
             }
+            while (num < 200)
+            {
+                num++;
 
+                numbers.Add(num);
+            }
 
             // This is to show the user that the numbers will start increasing on the console
             Console.WriteLine("Increase:");
 
 
             // Create a foreach loop using the collection - numbers
-                //In the scope of the foreach loop, print each number in numbers
-             
+            //In the scope of the foreach loop, print each number in numbers
+             foreach (var item in numbers) 
+            {
+                Console.WriteLine($"{item}");
+            }
 
 
             Console.WriteLine("");
@@ -62,8 +68,9 @@ namespace IterationStatements
                 // Decrement i by 1
 
             //start for loop here
+            for(int i =199; i <= numbers.Count && i >=0; i--)
             {
-                // place numbers[i] inside of the Console.WriteLine() method
+                Console.WriteLine(numbers[i]);
             }
 
             //------------End of exercise
